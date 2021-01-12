e1a55dfa3eb8848ac72521125e10dfcb6847f3a4
savrum <alp@savrum.com>
2021-01-12 22:32:22 +0300

    finalization

--- assets/mov-to-gif.sh
+++ assets/mov-to-gif.sh
@@ -1,1 +1,1 @@
-ffmpeg -ss 00:00:00.000 -i demo.mov -pix_fmt rgb24 -r 10 -s 1280x800 -t 00:00:10.000 demo.gif
+ffmpeg -ss 00:00:00.000 -i demo.mov -pix_fmt rgb24 -r 10 -s 1280x800 -t 00:00:20.000 demo.gif
